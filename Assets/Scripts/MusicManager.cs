62b93be319552b9bb001b735fe94dc6451f0a565
daisyqin123 <98994385+daisyqin123@users.noreply.github.com>
2022-09-16 09:41:05 +1000

    Audio

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip backgroundMusic;
+    public AudioSource audioSource;
+    
+    public AudioClip ghostNormalAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // introduction music play
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ghosts normal state play
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = ghostNormalAudio;
+            audioSource.Play();
+        }
+    }
+}
